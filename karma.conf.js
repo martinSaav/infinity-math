d80a6ce9bcf82be50f2c6882bfcdd4b3b1083c57
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-10-11 11:53:24 -0300

    otro intento de arreglar los test

--- karma.conf.js
+++ karma.conf.js
@@ -38,9 +38,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'], // Use ChromeHeadless for CI
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
